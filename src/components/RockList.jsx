cf339b0c0cf1a12fcab9ad3b98e32655331e3555
Tyler Osmond <140542331+tylerosmond@users.noreply.github.com>
2024-09-16 13:55:46 -0500

    added correct fetch funcion to useEffect() in RockList module and modifications to ApplicationViews() to handle user and label expands

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -1,23 +1,34 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 
 export const RockList = ({ rocks, fetchRocks }) => {
-    useEffect(() => {
-    }, [])
+  useEffect(() => {
+    fetchRocks();
+  }, []);
 
-    const displayRocks = () => {
-        if (rocks && rocks.length) {
-            return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                {rock.name} ({rock.type.label})
-            </div>)
-        }
-
-        return <h3>Loading Rocks...</h3>
+  const displayRocks = () => {
+    if (rocks && rocks.length) {
+      return rocks.map((rock) => (
+        <div
+          key={`key-${rock.id}`}
+          className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50"
+        >
+          <div>
+            {rock.name} ({rock.type.label})
+          </div>
+          <div>
+            In the collection of {rock.user.first_name} {rock.user.last_name}
+          </div>
+        </div>
+      ));
     }
 
-    return (
-        <>
-            <h1 className="text-3xl">Rock List</h1>
-            {displayRocks()}
-        </>
-    )
-}
+    return <h3>Loading Rocks...</h3>;
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl">Rock List</h1>
+      {displayRocks()}
+    </>
+  );
+};
